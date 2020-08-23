7b47fcc0239579f89b1897d23e3ef060e35bfbf9
Nathan Faucher <nfaucher@artsman.com>
2020-08-23 11:24:18 -0600

    Added cheese detail page

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
         route='',
         view=views.CheeseListView.as_view(),
         name='list'
+    ),
+    path(
+        route='<slug:slug>/',
+        view=views.CheeseDetailView.as_view(),
+        name='detail'
     )
 ]
